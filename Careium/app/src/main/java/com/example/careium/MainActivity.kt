ea0c5879cc289068878976cca8e3390a65238754
Kareem100 <KareemSherif210@gmail.com>
2022-02-23 07:23:56 +0200

    core: Adding Base Project Files Process 2

--- Careium/app/src/main/java/com/example/careium/MainActivity.kt
+++ Careium/app/src/main/java/com/example/careium/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.careium
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        findViewById<ImageButton>(R.id.image_menu).setOnClickListener{
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
     }
 }
