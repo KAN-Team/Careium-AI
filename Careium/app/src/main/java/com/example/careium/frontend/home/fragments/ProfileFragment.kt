b9eab6894b665b8afb97b24e1a4504283714a339
kareem saeed <karimsaid026@gmail.com>
2022-03-16 02:00:49 +0200

    merge-conflict: merge the two branches K_SAEED with main

--- Careium/app/src/main/java/com/example/careium/frontend/home/fragments/ProfileFragment.kt
+++ Careium/app/src/main/java/com/example/careium/frontend/home/fragments/ProfileFragment.kt
@@ -2,18 +2,26 @@ package com.example.careium.frontend.home.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.TextView
 import com.example.careium.R
+import com.example.careium.databinding.FragmentProfileBinding
 
-class ProfileFragment : Fragment() {
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private lateinit var binding: FragmentProfileBinding
 
-        val editProfileText: TextView = requireView().findViewById(R.id.profile_edit_button)
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            ProfileFragment().apply {}
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentProfileBinding.bind(view)
+
+        val editProfileText: TextView = binding.profileEditButton
         editProfileText.setOnClickListener {
             //TODO: activate editing user profile by enable editing EditViews in profile fragment layout
 
@@ -21,18 +29,4 @@ class ProfileFragment : Fragment() {
 
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
-    }
-
-    companion object {
-        @JvmStatic
-        fun newInstance() =
-            ProfileFragment().apply {
-
-            }
-    }
 }
