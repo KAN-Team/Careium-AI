f76d9db80d544923893ed85ca226317999f28c79
kareem saeed <karimsaid026@gmail.com>
2022-04-01 01:04:28 +0200

    frontend: Adding Splash Screen UI.

--- Careium/app/src/main/java/com/example/careium/frontend/authentication/activities/SplashActivity.kt
+++ Careium/app/src/main/java/com/example/careium/frontend/authentication/activities/SplashActivity.kt
@@ -1,17 +1,34 @@
 package com.example.careium.frontend.authentication.activities
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.example.careium.databinding.ActivitySplashBinding
+import com.example.careium.frontend.home.activities.MainActivity
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashBinding
+    private val x: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        // Handle Here Authentication Condition to start main activity
+        if (x == 1) {
+            binding.loginBtn.visibility = View.GONE
+            binding.registerBtn.visibility = View.GONE
+            Handler().postDelayed({
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }, 100)
+        }
 
 
         binding.loginBtn.setOnClickListener {
