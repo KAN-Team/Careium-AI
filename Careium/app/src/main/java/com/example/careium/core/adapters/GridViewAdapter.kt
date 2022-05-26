af7c5c1aed29d0b4e30431a94511f0719616414d
Nada elsayed anies <nada.said39@gmail.com>
2022-05-26 04:06:18 +0200

    add dish image item and adapter of the grid view

---
+++ Careium/app/src/main/java/com/example/careium/core/adapters/GridViewAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.careium.core.adapters
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Bitmap
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.careium.R
+
+internal class GridViewAdapter(
+    private val context: Context,
+    private val numberImage: ArrayList<Bitmap>
+
+) :
+    BaseAdapter() {
+    private var layoutInflater: LayoutInflater? = null
+    private lateinit var imageView: ImageView
+
+    override fun getCount(): Int {
+        return numberImage.size
+    }
+
+    override fun getItem(position: Int): Any? {
+        return null
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    @SuppressLint("InflateParams")
+    override fun getView(
+        position: Int,
+        convertView: View?,
+        parent: ViewGroup
+    ): View {
+        var convertView = convertView
+        if (layoutInflater == null) {
+            layoutInflater =
+                context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        }
+        if (convertView == null) {
+            convertView = layoutInflater!!.inflate(R.layout.dish_image_item, null)
+        }
+        imageView = convertView!!.findViewById(R.id.imageView)
+        imageView.setImageBitmap(numberImage[position])
+        return convertView
+    }
+}
